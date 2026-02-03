3031f42ab32c333a6ecd892f3cf601ef2033cdf6
Ishan Savarna <jeetishan51@gmail.com>
2026-02-03 16:45:25 +0530

    Day -2

--- script.js
+++ script.js
@@ -3,11 +3,20 @@
 //const timeoutId=setTimeout(()=>{clear setInterval(timerID)},10000)
 // clearTimeout(timeoutID)
 
-let num=1;
-const id=setInterval(()=>{
-    if(num==10)clearInterval(id)
-    console.log(num)
-    num+=1
-},1000)
+//let num=1;
+//const id=setInterval(()=>{
+//    if(num==10)clearInterval(id)
+//    console.log(num)
+//    num+=1
+//},1000)
 
+const name=document.querySelectorAll("#name")
+const btn=document.querySelectorAll(".btn")
+const list=document.querySelectorAll(".list")
+btn.addEventlistener('click',()=>{
+    const li=document.createElement('li')
+    li.innerText=name.value;
+    list.appendChild(li)
+    name.value="1000"
+})
 
