62b4d01375aa6c6395087e0f33fe7df527d72713
Ishan Savarna <jeetishan51@gmail.com>
2026-02-06 12:39:04 +0530

    3

--- script.js
+++ script.js
@@ -10,13 +10,38 @@
 //    num+=1
 //},1000)
 
-const name=document.querySelectorAll("#name")
-const btn=document.querySelectorAll(".btn")
-const list=document.querySelectorAll(".list")
-btn.addEventlistener('click',()=>{
-    const li=document.createElement('li')
-    li.innerText=name.value;
-    list.appendChild(li)
-    name.value="1000"
-})
+// const name=document.querySelectorAll("#name")
+// const btn=document.querySelectorAll(".btn")
+// const list=document.querySelectorAll(".list")
+// btn.addEventlistener('click',()=>{
+//     const li=document.createElement('li')
+//     const dlt=document.createElement('buttons')
+//     dlt.innerText="DELETE"
+//     li.innerText=name.value;
+//     dlt.addEventListener("CLICK",()=>{
+//         list.removechild(li)
+//     })
+//     li.appendChild(dlt)
+//     list.appendChild(li)
+//     name.value=""
+// })
+// console.log("starting homework....")
+// setTimeout(() => {
+//     console.log("homework done")
+//     console.log("starting dinner")
+//     setTimeout(() => {
+//         console.log("dinner done")
+//         console.log("getting ready to go out....")
+//         setTimeout(() => {
+        
+//     }, timeout);
+
+// }, timeout);
+// })
+
+function finishHomework (callback) {}
+
+
+
+
 
